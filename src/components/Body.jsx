7616d355d554729bc488d10a835f0d59bdf9890a
Altamash Raza <altamashraza3.ar@gmail.com>
2025-10-20 15:30:17 +0530

    body.jsx

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,28 +1,21 @@
+import React from "react";
 import Login from "./Login";
 import Browse from "./Browse";
-import { createBrowserRouter, RouterProvider} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const Body = () => {
-
-  const appRouter = createBrowserRouter([
+  // Tell the router about your app base path
+  const appRouter = createBrowserRouter(
+    [
+      { path: "/", element: <Login /> },
+      { path: "/browse", element: <Browse /> },
+    ],
     {
-      path: "/",
-      element: <Login />,
-    },
-    {
-      path: "/browse",
-      element: <Browse />,
-    },
-  ]);
-
-
-
-
-  return (
-    <div>
-      <RouterProvider router={appRouter}/>
-    </div>
+      basename: "/netflix-gpt", // <-- important for deployment under subpath
+    }
   );
+
+  return <RouterProvider router={appRouter} />;
 };
 
 export default Body;
